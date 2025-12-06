08db47c15f7f4b320868dd99ba899b5d499d6dba
prashant4840 <prashantsharma4830@gmail.com>
2025-12-06 16:30:38 +0530

    final commit

---
+++ game.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+/*for bg sound*/
+#include <mmsystem.h>
+
+/*to remove flicker*/
+void clear_screen_fast() {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD pos = {0, 0};
+    SetConsoleCursorPosition(h, pos);
+}
+
+int main() {
+	system("color 4F");
+	PlaySound(TEXT("bg.wav"), NULL, SND_ASYNC | SND_LOOP);
+    srand(time(0));
+
+    int x = 1;              // player position (0 to 2)
+    int step = 0;           // obstacle vertical movement
+    int obstaclePos = rand() % 3;   // 0,1,2 lane
+
+    while (1) {
+
+        // ---- INPUT ----
+        if (_kbhit()) {
+            char ch = getch();
+
+            if (ch == 75 && x > 0)        // LEFT arrow
+                x--;
+
+            if (ch == 77 && x < 2)        // RIGHT arrow
+                x++;
+        }
+
+        // ---- DRAW ----
+		// system("cls");
+		clear_screen_fast();
+        printf("|--- --- ---|\n");
+
+        for (int i = 0; i < 10; i++) {
+            if (i == step) {
+
+                if (obstaclePos == 0)
+                    printf("| %c        |\n", 1);
+
+                else if (obstaclePos == 1)
+                    printf("|     %c    |\n", 1);
+
+                else if (obstaclePos == 2)
+                    printf("|        %c |\n", 1);
+
+            } else {
+                printf("|           |\n");
+            }
+        }
+
+        // ---- PLAYER ----
+        if (x == 0)
+            printf("| %c         |\n", 6);
+        else if (x == 1)
+            printf("|     %c     |\n", 6);
+        else if (x == 2)
+            printf("|        %c  |\n", 6);
+
+        // ---- COLLISION ----
+        if (step == 10 && x == obstaclePos) {
+        	PlaySound(NULL, NULL, 0);  // stop background
+			PlaySound(TEXT("impact.wav"), NULL, SND_ASYNC);
+        	Sleep(2500);
+            printf("\nGAME OVER!\n");
+            break;
+        }
+
+        Sleep(120);
+
+        // Move obstacle down
+        step++;
+
+        // Reset when reaches bottom
+        if (step > 10) {
+            step = 0;
+            obstaclePos = rand() % 3; // new lane
+        }
+    }
+
+    return 0;
+}
